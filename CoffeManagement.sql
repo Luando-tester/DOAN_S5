d7d8a8537270568dd1920c687687a8de6badaf82
Lai <nguyenduclai1997@gmail.com>
2018-11-11 05:18:44 -0800

    Tao bang database
    tao table ban nuoc

--- CoffeManagement.sql
+++ CoffeManagement.sql
@@ -19,3 +19,11 @@ CREATE TABLE Account
 	Type INT NOT NULL DEFAULT 0 --loai tai khoan -- 0:Quan ly -- 1:Nhan vien
 )
 GO
+--Tao table ban nuoc
+CREATE TABLE TableDrink
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100)NOT NULL DEFAULT N'chua dat ten',
+	status NVARCHAR(100) NOT NULL DEFAULT N'Trong' --Status ban trong hoac co nguoi
+)
+GO
